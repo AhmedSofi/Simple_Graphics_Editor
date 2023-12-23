653de8944fee5edb86d4a326e08378070429f40a
maielshamy <111990929+maielshamy@users.noreply.github.com>
2023-12-23 16:53:52 +0200

    Update main.py

--- function/main.py
+++ function/main.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib as plt
-
+import matplotlib.pyplot as plt
 
 class Editor:
     def __init__(self, image_path):
@@ -18,13 +17,12 @@ class Editor:
 
     def convert2grayscale(self):
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-    
+
     def color_elimination(self, color):
         if len(self.image.shape) == 2:
             print("The image is already in grayscale.")
             return
 
-        # Define color channel elimination based on the desired color
         if color == "red":
             self.image[:, :, 2] = 0  # Eliminate red channel
         elif color == "green":
@@ -50,7 +48,6 @@ class Editor:
 
         img_rgb = self.image.copy()
 
-        # Return the original image for RGB order
         if channel_order == 'RGB':
             return img_rgb
         elif channel_order == 'GRB':
@@ -64,134 +61,96 @@ class Editor:
 
         self.image = img_rgb
 
-
     def complement_image(self):
         if len(self.image.shape) == 2:
-            # Grayscale image complement
             self.image = 255 - self.image
         else:
-            # convert from BGR to Grayscale
             self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-            # Grayscale image complement
             self.image = 255 - self.image
 
     def solarize_image(self, threshold=120):
         if len(self.image.shape) == 2:
-            # Grayscale image solarization
             self.image[self.image < threshold] = self.image[self.image < threshold]
             self.image[self.image >= threshold] = 255 - self.image[self.image >= threshold]
         else:
-            # convert from BGR to Grayscale
             self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-            # Grayscale image solarization
             self.image[self.image < threshold] = self.image[self.image < threshold]
             self.image[self.image >= threshold] = 255 - self.image[self.image >= threshold]
 
     def darken_image(self, darkening_factor=0.2):
-        # Darken the image by multiplying all pixel values by the darkening factor
         if len(self.image.shape) == 2:
             self.image = (self.image * darkening_factor).astype(np.uint8)
         else:
-            # convert from BGR to Grayscale
             self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
             self.image = (self.image * darkening_factor).astype(np.uint8)
 
     def brighten_image(self, brightening_factor=50):
-        # Brighten the image by adding all pixel values by the brightening factor
         if len(self.image.shape) == 2:
             self.image = cv2.add(self.image, brightening_factor).astype(np.uint8)
-
         else:
-            # convert from BGR to Grayscale
             self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
             self.image = cv2.add(self.image, brightening_factor).astype(np.uint8)
 
- 
+    def adjust_brightness(self, brightness_value):
+        modified_image = cv2.add(self.image, brightness_value)
+        modified_image = cv2.cvtColor(modified_image, cv2.COLOR_BGR2RGB)
+        return modified_image
 
-    def adjust_brightness(image, brightness_value):
-             modified_image = cv2.add(image, brightness_value)
-             modified_image = cv2.cvtColor(modified_image, cv2.COLOR_BGR2RGB)  # Convert to RGB for display
-            return modified_image
+    def adjust_color(self, color, adjustment_value):
+        modified_image = self.image.copy()
 
-    def adjust_color(image, color, adjustment_value):
-             modified_image = image.copy()
+        if color == 'Red':
+            modified_image[:, :, 2] = cv2.add(modified_image[:, :, 2], adjustment_value)
+        elif color == 'Green':
+            modified_image[:, :, 1] = cv2.add(modified_image[:, :, 1], adjustment_value)
+        elif color == 'Blue':
+            modified_image[:, :, 0] = cv2.add(modified_image[:, :, 0], adjustment_value)
+        elif color == 'Yellow':
+            modified_image[:, :, 2] = cv2.add(modified_image[:, :, 2], adjustment_value)
+            modified_image[:, :, 1] = cv2.add(modified_image[:, :, 1], adjustment_value)
+        elif color == 'Purple':
+            modified_image[:, :, 2] = cv2.add(modified_image[:, :, 2], adjustment_value)
+            modified_image[:, :, 0] = cv2.add(modified_image[:, :, 0], adjustment_value)
+        elif color == 'Cyan':
+            modified_image[:, :, 1] = cv2.add(modified_image[:, :, 1], adjustment_value)
+            modified_image[:, :, 0] = cv2.add(modified_image[:, :, 0], adjustment_value)
+        else:
+            print("Invalid color option.")
 
-         if color == 'Red':
-             modified_image[:, :, 2] = cv2.add(modified_image[:, :, 2], adjustment_value)  # Increase intensity of red channel
-         elif color == 'Green':
-             modified_image[:, :, 1] = cv2.add(modified_image[:, :, 1], adjustment_value)  # Increase intensity of green channel
-         elif color == 'Blue':
-             modified_image[:, :, 0] = cv2.add(modified_image[:, :, 0], adjustment_value)  # Increase intensity of blue channel
-         elif color == 'Yellow':
-             modified_image[:, :, 2] = cv2.add(modified_image[:, :, 2], adjustment_value)  # Increase intensity of red channel
-             modified_image[:, :, 1] = cv2.add(modified_image[:, :, 1], adjustment_value)  # Increase intensity of green channel
-         elif color == 'Purple':
-             modified_image[:, :, 2] = cv2.add(modified_image[:, :, 2], adjustment_value)  # Increase intensity of red channel
-             modified_image[:, :, 0] = cv2.add(modified_image[:, :, 0], adjustment_value)  # Increase intensity of blue channel
-         elif color == 'Cyan':
-             modified_image[:, :, 1] = cv2.add(modified_image[:, :, 1], adjustment_value)   
-             modified_image[:, :, 0] = cv2.add(modified_image[:, :, 0], adjustment_value)   
+        modified_image = cv2.cvtColor(modified_image, cv2.COLOR_BGR2RGB)
+        return modified_image
 
-             modified_image = cv2.cvtColor(modified_image, cv2.COLOR_BGR2RGB)   
-               return modified_image
+    def plot_histogram(self, channels='BGR'):
+        image = self.image.copy()
+        if channels == 'BGR':
+            color = ('b', 'g', 'r')
+            for i, col in enumerate(color):
+                histogram = cv2.calcHist([image], [i], None, [256], [0, 256])
+                plt.plot(histogram, color=col)
+                plt.xlim([0, 256])
+            plt.xlabel('Pixel Value')
+            plt.ylabel('Frequency')
+            plt.title('BGR Image Histogram')
+        elif channels == 'Grayscale':
+            if len(image.shape) == 3:
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
+            plt.plot(histogram, color='black')
+            plt.xlabel('Pixel Value')
+            plt.ylabel('Frequency')
+            plt.title('Grayscale Image Histogram')
+        elif channels in ['Red', 'Green', 'Blue']:
+            channel_idx = {'Red': 2, 'Green': 1, 'Blue': 0}
+            idx = channel_idx[channels]
+            histogram = cv2.calcHist([image], [idx], None, [256], [0, 256])
+            plt.plot(histogram, color=channels.lower())
+            plt.xlabel('Pixel Value')
+            plt.ylabel('Frequency')
+            plt.title(f'{channels} Channel Histogram')
+        else:
+            print("Invalid channel option.")
 
- 
-           brightened_image = adjust_brightness(img_original, 50)
-
-               colored_image_blue = adjust_color(img_original, 'Blue', 50)
-               colored_image_green = adjust_color(img_original, 'Green', 50)
-               colored_image_cyan = adjust_color(img_original, 'Cyan', 50)
-               colored_image_purple = adjust_color(img_original, 'Purple', 50)
-               colored_image_yellow = adjust_color(img_original, 'Yellow', 50)
-
-               cv2.imshow('Original Image', img_original)
-               cv2.imshow('Brightened Image', brightened_image)
-               cv2.imshow('Colored Image (Blue)', colored_image_blue)
-               cv2.imshow('Colored Image (Green)', colored_image_green)
-               cv2.imshow('Colored Image (Cyan)', colored_image_cyan)
-               cv2.imshow('Colored Image (Purple)', colored_image_purple)
-               cv2.imshow('Colored Image (Yellow)', colored_image_yellow)
-               cv2.waitKey(0)
-               cv2.destroyAllWindows()
-
-
-
-
-     
-  import matplotlib.pyplot as plt
-
-    def plot_histogram(image, channels='BGR'):
-       if channels == 'BGR':
-        color = ('b', 'g', 'r')
-          for i, col in enumerate(color):
-            histogram = cv2.calcHist([image], [i], None, [256], [0, 256])
-            plt.plot(histogram, color=col)
-            plt.xlim([0, 256])
-          plt.xlabel('Pixel Value')
-          plt.ylabel('Frequency')
-          plt.title('BGR Image Histogram')
-       elif channels == 'Grayscale':
-          histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
-          plt.plot(histogram, color='black')
-          plt.xlabel('Pixel Value')
-          plt.ylabel('Frequency')
-          plt.title('Grayscale Image Histogram')
-       elif channels in ['Red', 'Green', 'Blue']:
-          channel_idx = {'Red': 2, 'Green': 1, 'Blue': 0}
-          idx = channel_idx[channels]
-          histogram = cv2.calcHist([image], [idx], None, [256], [0, 256])
-          plt.plot(histogram, color=channels.lower())
-          plt.xlabel('Pixel Value')
-          plt.ylabel('Frequency')
-         plt.title(f'{channels} Channel Histogram')
-
- 
-
- 
-         plot_histogram(img, channels='BGR')
- 
-
-         plt.show()
+        plt.show()
 
 
 
